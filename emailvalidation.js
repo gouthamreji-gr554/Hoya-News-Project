4efe45745beadbf7a39fbc7d63b8b8dd3e55f8f4
ac1956 <ac1956@users.noreply.github.com>
2020-02-20 02:54:08 -0500

    Commiting final changes

--- emailvalidation.js
+++ emailvalidation.js
@@ -1,10 +1,10 @@
 function validate() {
-    var Email = document.getElementById("Email").value;
+    var Email = document.getElementById("t-email").value;
     var patcheck = new RegExp("^[a-z0-9]");
     if (Email.indexOf("@") > -1) {
         document.getElementById("div2").innerHTML = "";
     } else {
-        document.getElementById("div2").innerHTML = "Enter the correct email address";
+        document.getElementById("div2").innerHTML = "Enter a valid email address";
         document.getElementById("div2").style.color = "Red";
 
     }
@@ -12,6 +12,12 @@ function validate() {
 
 $(function() {
     $("#t-btn").click(function() {
-        window.location.href = "https://ac1956.github.io/Hoya-News-Project/News.html";
+        if (!$("#t-email").val() || !$("#t-pass").val()) {
+            document.getElementById("div2").innerHTML = "Enter email and password";
+            document.getElementById("div2").style.color = "Red";
+        } else {
+            window.location.href = "https://ac1956.github.io/Hoya-News-Project/News.html";
+        }
+
     });
 });
